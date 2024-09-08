e6b3832ee954cdb70269cd4f3e61ab48249d1a77
TusharIbtekar <ibtekar8@gmail.com>
2024-09-08 13:04:53 +0600

    feat: update assembly code generator to masm

--- src/code_generator.rs
+++ src/code_generator.rs
@@ -4,97 +4,101 @@ pub fn generate_output_code(ir: &[IRInstruction]) -> String {
   let mut output = String::new();
   let mut label_counter = 0;
 
-  output += "    .data\n";
+  output += ".data\n\n";
 
-  output += "    .bss\n";
-  output += "    .lcomm stack_pointer, 8\n\n";
+  output += ".bss\n";
+  output += "    stack_pointer DWORD ?\n\n";
 
-  output += "    .text\n";
-  output += "    .globl main\n\n";
-  output += "main:\n";
+  output += ".code\n";
+  output += "global main\n\n";
 
-  output += "    pushq %rbp\n";
-  output += "    movq %rsp, %rbp\n";
-  output += "    subq $256, %rsp  # Allocate 256 bytes for local variables\n\n";
+  output += "main PROC\n";
+
+  output += "    push ebp\n";
+  output += "    mov ebp, esp\n";
+  output += "    sub esp, 256  ; Allocate 256 bytes for local variables\n\n";
 
   for (i, instruction) in ir.iter().enumerate() {
     match instruction {
       IRInstruction::LoadConstant(n) => {
-        output += &format!("    movq ${}, %rax\n", n);
-        output += "    pushq %rax\n";
+        output += &format!("    mov eax, {}\n", n);
+        output += "    push eax\n";
       }
       IRInstruction::LoadVariable(var) => {
-        output += &format!("    movq {}(%rbp), %rax\n", get_variable_offset(var));
-        output += "    pushq %rax\n";
+        output += &format!("    mov eax, [ebp+{}]\n", get_variable_offset(var));
+        output += "    push eax\n";
       }
       IRInstruction::Add => {
-        output += "    popq %rbx\n";
-        output += "    popq %rax\n";
-        output += "    addq %rbx, %rax\n";
-        output += "    pushq %rax\n";
+        output += "    pop ebx\n";
+        output += "    pop eax\n";
+        output += "    add eax, ebx\n";
+        output += "    push eax\n";
       }
       IRInstruction::Subtract => {
-        output += "    popq %rbx\n";
-        output += "    popq %rax\n";
-        output += "    subq %rbx, %rax\n";
-        output += "    pushq %rax\n";
+        output += "    pop ebx\n";
+        output += "    pop eax\n";
+        output += "    sub eax, ebx\n";
+        output += "    push eax\n";
       }
       IRInstruction::Multiply => {
-        output += "    popq %rbx\n";
-        output += "    popq %rax\n";
-        output += "    imulq %rbx, %rax\n";
-        output += "    pushq %rax\n";
+        output += "    pop ebx\n";
+        output += "    pop eax\n";
+        output += "    imul eax, ebx\n";
+        output += "    push eax\n";
       }
       IRInstruction::Divide => {
-        output += "    popq %rbx\n";
-        output += "    popq %rax\n";
-        output += "    cqo\n";
-        output += "    idivq %rbx\n";
-        output += "    pushq %rax\n";
+        output += "    pop ebx\n";
+        output += "    pop eax\n";
+        output += "    cdq\n";
+        output += "    idiv ebx\n";
+        output += "    push eax\n";
       }
       IRInstruction::Store(var) => {
-        output += "    popq %rax\n";
-        output += &format!("    movq %rax, {}(%rbp)\n", get_variable_offset(var));
+        output += "    pop eax\n";
+        output += &format!("    mov [ebp+{}], eax\n", get_variable_offset(var));
       }
       IRInstruction::Compare(op) => {
-        output += "    popq %rbx\n";
-        output += "    popq %rax\n";
-        output += "    cmpq %rbx, %rax\n";
+        output += "    pop ebx\n";
+        output += "    pop eax\n";
+        output += "    cmp eax, ebx\n";
         match op.as_str() {
-          "==" => output += "    sete %al\n",
-          "<" => output += "    setl %al\n",
-          ">" => output += "    setg %al\n",
-          "<=" => output += "    setle %al\n",
-          ">=" => output += "    setge %al\n",
+          "==" => output += "    sete al\n",
+          "<" => output += "    setl al\n",
+          ">" => output += "    setg al\n",
+          "<=" => output += "    setle al\n",
+          ">=" => output += "    setge al\n",
           _ => panic!("Invalid comparison operator"),
         }
-        output += "    movzbq %al, %rax\n";
-        output += "    pushq %rax\n";
+        output += "    movzx eax, al\n";
+        output += "    push eax\n";
       }
       IRInstruction::JumpIfFalse(label) => {
-        output += "    popq %rax\n";
-        output += "    testq %rax, %rax\n";
-        output += &format!("    je .L{}\n", label);
+        output += "    pop eax\n";
+        output += "    test eax, eax\n";
+        output += &format!("    je L{}:\n", label);
         label_counter = label_counter.max(*label + 1);
       }
       IRInstruction::Jump(label) => {
-        output += &format!("    jmp .L{}\n", label);
+        output += &format!("    jmp L{}:\n", label);
         label_counter = label_counter.max(*label + 1);
       }
     }
-    if i + 1 == label_counter {
-      output += &format!(".L{}:\n", i + 1);
+    if label_counter > 0 {
+      output += &format!("L{}:\n", label_counter);
     }
   }
 
-  output += "\n    # Function epilogue\n";
-  output += "    movq $0, %rax  # Return 0\n";
-  output += "    leave\n";
+  output += "\n    mov eax, 0  ; Exit code 0\n";
+  output += "    mov esp, ebp\n";
+  output += "    pop ebp\n";
   output += "    ret\n";
+
+  output += "main ENDP\n";
+  output += "END main\n";
 
   output
 }
 
 fn get_variable_offset(var: &str) -> String {
-  format!("-{}(%rbp)", var.chars().next().unwrap() as u32 * 8) // simplified
+  format!("{}", (var.chars().next().unwrap() as u32) * 8) // Simplified
 }
